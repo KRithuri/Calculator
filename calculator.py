f25c5856977ff6f3e4353ae64a70850f00110b42
Karabo Rithuri <rithurikarabo@gmail.com>
2023-08-20 21:07:24 +0200

    print options to the user

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+# define functions needed
 def add(x, y):
     answer = x + y
     return answer
@@ -13,3 +14,6 @@ def multiplication(x, y):
 def division(x, y):
     answer = x / y
     return answer
+
+# print options to the user
+print("Select operation: \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide")
